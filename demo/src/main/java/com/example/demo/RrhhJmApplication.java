0a880c2959d9fed4627a78e40bafc944784403e9
MateoGC-04 <mateogonzalezcano04@gmail.com>
2024-09-14 15:19:13 -0500

    SpringAdd

---
+++ demo/src/main/java/com/example/demo/RrhhJmApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrhhJmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RrhhJmApplication.class, args);
+	}
+
+}
